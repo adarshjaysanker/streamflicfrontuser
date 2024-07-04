caee94c27ea4434ac0b71b2c3d44ae738c614df2
adarsh <adarshjayasanker5@gmail.com>
2024-07-04 08:47:51 +0530

    fetch random movie in the banner container and info and movie playable

--- src/components/Bannercontainer/Bannercontainer.jsx
+++ src/components/Bannercontainer/Bannercontainer.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import './bannercontainer.css'
+import { useNavigate } from 'react-router-dom';
 
-function Bannercontainer() {
+function Bannercontainer({randomMovie}) {
+
+  const navigate = useNavigate();
+
+  if(!randomMovie){
+    return <div>Loading .....</div>
+  }
+
+  const backgroundImageStyle = {
+    backgroundImage: `url(${randomMovie.infoBanner})`
+  }
+
+  const handlePlayClick = () => {
+    const encodedMoviePath = encodeURIComponent(randomMovie.movieVideo);
+    navigate(`/watch/${encodedMoviePath}`)
+  }
+
+  const handleInfoClick = () => {
+    navigate(`movie/${randomMovie._id}`);
+  }
+
   return (
-    <div className='banner-container'>
+    <div className='banner-container' style={backgroundImageStyle}>
       <div className="banner-content">
-        <h1 className='banner-title'>Breaking Bad</h1>
-        <p className="banner-description">A chemistry teacher diagnosed with inoperable lung cancer turns to manufacturing and selling methamphetamine with a former student in order to secure his family's future.</p>
+        <h1 className='banner-title'>{randomMovie.title}</h1>
+        <p className="banner-description">{randomMovie.description}</p>
         <div className="banner-buttons">
-            <button className="banner-button info">Info</button>
-            <button className="banner-button play">Play</button>
+            <button className="banner-button info" onClick={handleInfoClick}>Info</button>
+            <button className="banner-button play" onClick={handlePlayClick}>Play</button>
         </div>
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Bannercontainer from '../components/Bannercontainer/Bannercontainer'
 import Rowcontainer from '../components/Rowcontainer/Rowcontainer'
 import Header from '../components/Header/Header'
@@ -9,43 +9,74 @@ function Home() {
 
   const navigate = useNavigate();
 
+  const [randomMovie, setRandomMovie] = useState(null);
+
   const handleLogout = () => {
     localStorage.removeItem('authToken');
     navigate('/login');
   };
 
-  useEffect(() => {
-    const fetchHome = async() => {
-      try{
-        const token = localStorage.getItem('authToken');
-        if(!token){
-          navigate('/login');
-          return;
-        }
-        const res = await fetch('https://api.streamflics.xyz/gethome',{
-          method: 'GET',
-          headers: {
-            'Authorization': `Bearer ${token}`
-          },
-        });
-        if(res.ok){
-          const data = await res.json();
-          console.log(data.message);
-        }else{
-          navigate('/login');
-        }
-      }catch(error){
-        console.error(error);
+  const fetchHome = async() => {
+    try{
+      const token = localStorage.getItem('authToken');
+      if(!token){
+        navigate('/login');
+        return;
+      }
+      const res = await fetch('http://localhost:5000/gethome',{
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`
+        },
+      });
+      if(res.ok){
+        const data = await res.json();
+        console.log(data.message);
+      }else{
         navigate('/login');
       }
+    }catch(error){
+      console.error(error);
+      navigate('/login');
     }
+  };
+
+  const fetchRandomMovie = async() => {
+    try{
+      const token = localStorage.getItem('authToken');
+      if(!token){
+        navigate('/login');
+        return;
+      }
+      const res = await fetch('http://localhost:5000/getrandommovie',{
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`
+        },
+      });
+      if(res.ok){
+        const data = await res.json();
+        setRandomMovie(data.movie);
+        console.log(data.movie);
+      }
+    }catch(error){
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
     fetchHome();
+    fetchRandomMovie();
+    const intervalid = setInterval(() => {
+      fetchRandomMovie();
+    }, 3600000);
+    return () => clearInterval(intervalid);
   }, [navigate])
 
   return (
     <div>
       <Header logout={handleLogout}/>
-      <Bannercontainer/>
+      <Bannercontainer randomMovie={randomMovie}/>
       <Rowcontainer/>
     </div>
   )
